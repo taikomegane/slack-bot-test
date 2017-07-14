f775ab58af5b3ee4e7708dfe298349590df6d9d6
taikomegane <taiko.megane.gobou@gmail.com>
2017-07-14 23:13:54 +0900

    test

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 # botアカウントのトークンを指定
-API_TOKEN = "xoxb-211822155441-R4iamIZNtCPgqtX6wuDiD1Cl"
+API_TOKEN = "xoxb-211822155441-tMeIRrjCXZtlHO170ikCWhk6"
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "リプライを確認しました"
